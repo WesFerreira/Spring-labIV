47ed7adc7589427bd97748b570779b7ab6780e66
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-14 20:21:18 -0300

    Commit inicial, estrutura do projeto

---
+++ src/main/java/br/com/wesley/springlabiv/SpringLabivApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesley.springlabiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wesley/springlabiv/SpringLabivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesley.springlabiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
